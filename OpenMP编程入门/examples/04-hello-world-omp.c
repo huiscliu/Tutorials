b0893ac319a5d0e6bb01a075ee7dde15d2185644
huiscliu <username@users.noreply.github.com>
2020-09-05 10:17:50 -0600

    add hello world

---
+++ OpenMP编程入门/examples/04-hello-world-omp.c
@@ -0,0 +1,12 @@
+#include <omp.h>
+#include <stdio.h>
+
+int main()
+{
+#pragma omp parallel
+    {
+        printf("Hello World!\n");
+    }
+
+    return 0;
+}
---
+++ OpenMP编程入门/examples/04-hello-world.c
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World!\n");
+
+    return 0;
+}
