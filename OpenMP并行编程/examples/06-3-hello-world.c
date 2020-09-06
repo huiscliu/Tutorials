08d249e247a55fc5b46f5a3e387a54b8a88ce78f
huiscliu <username@users.noreply.github.com>
2020-09-06 15:03:53 -0600

    get id

---
+++ OpenMP并行编程/examples/06-3-hello-world.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include <omp.h>
+
+int main()
+{
+#pragma omp parallel
+    {
+        printf("Hello World, thread number: %d\n", omp_get_thread_num());
+    }
+
+    return 0;
+}
