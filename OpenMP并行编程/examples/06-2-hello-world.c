8b14cf0b3ffac5a15b7d5a1e2f52e624d1b24c4e
huiscliu <username@users.noreply.github.com>
2020-09-06 14:07:57 -0600

    add get

---
+++ OpenMP并行编程/examples/06-2-hello-world.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main()
+{
+    printf("serial part, total number of threads: %d\n\n", omp_get_num_threads());
+
+#pragma omp parallel
+    {
+        printf("Hello World, total number of threads: %d\n", omp_get_num_threads());
+    }
+
+    return 0;
+}
