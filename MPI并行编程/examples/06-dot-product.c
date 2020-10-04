db2dcba9d4f1038cc3c5fe86611e1d3ed02b78fe
huiscliu <username@users.noreply.github.com>
2020-10-04 17:41:50 -0600

    add dot product

---
+++ MPI并行编程/examples/06-dot-product.c
@@ -0,0 +1,66 @@
+
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* rank 0 computes L2 norm of a vector
+ * length is 1 million
+ * v[i] = i */
+int main(int argc, char **argv)
+{
+    MPI_Comm comm = MPI_COMM_WORLD;
+    int rank, nprocs;
+
+    int size = 1000000;
+    double *x = NULL;
+    int lb;    /* lower bound */
+    int ub;    /* upper bound */
+    int lsize; /* local work size */
+    int i;
+    double nrm = 0.;
+
+    MPI_Status status;
+
+    /* initialize */
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_size(comm, &nprocs);
+    MPI_Comm_rank(comm, &rank);
+
+    /* vector index distribution */
+    lsize = size / nprocs;
+    lb = lsize * rank;
+    ub = lsize * (rank + 1);
+    if (rank == nprocs - 1) ub = size;
+
+    /* local work */
+    lsize = ub - lb;
+    x = malloc(lsize * sizeof(*x));
+    for (i = 0; i < lsize; i++) x[i] = i + lb;
+
+    /* partial norm */
+    for (i = 0; i < lsize; i++) nrm += x[i] * x[i];
+
+    /* rank 0 receives partial norm from other MPIs */
+    if (rank == 0) {
+        double temp;
+
+        for (i = 1; i < nprocs; i++) {
+            MPI_Recv(&temp, 1, MPI_DOUBLE, i, 2020, comm, &status);
+
+            nrm += temp;
+        }
+
+        nrm = sqrt(nrm);
+        printf("L2 norm is %g\n", nrm);
+    }
+    else {
+        MPI_Send(&nrm, 1, MPI_DOUBLE, 0, 2020, comm);
+    }
+
+    free(x);
+    MPI_Finalize();
+
+    return 0;
+}
