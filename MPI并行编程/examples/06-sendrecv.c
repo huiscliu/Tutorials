c101a0bc921a175f2abcb89a5039eb4a0a3203cf
huiscliu <username@users.noreply.github.com>
2020-10-04 17:10:38 -0600

    add sendrecv

---
+++ MPI并行编程/examples/06-sendrecv.c
@@ -0,0 +1,43 @@
+
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    MPI_Comm comm = MPI_COMM_WORLD;
+    int rank, nprocs;
+    int sb[4];
+    int rb[4] = {0, 0, 0, 0};
+    MPI_Status status;
+
+    /* initialize */
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_size(comm, &nprocs);
+    MPI_Comm_rank(comm, &rank);
+
+    if (nprocs < 2) {
+        printf("At least 2 MPIs are required for this example\n");
+        goto end;
+    }
+
+    /* process 0 send info to 1 */
+    if (rank == 0) {
+        sb[0] = 0;
+        sb[1] = 1;
+        sb[2] = 2;
+        sb[3] = 3;
+
+        MPI_Send(sb, 2, MPI_INT, 1, 999, comm);
+    }
+    else if (rank == 1) {
+        MPI_Recv(rb, 2, MPI_INT, 0, 999, comm, &status);
+
+        printf("received: %d %d\n", rb[0], rb[1]);
+    }
+
+end:
+    MPI_Finalize();
+
+    return 0;
+}
