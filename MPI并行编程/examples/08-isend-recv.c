673c3b2ed9ccacff25bd882f83a876a9261b0ec8
huiscliu <username@users.noreply.github.com>
2020-10-29 22:05:57 -0600

    add non-blocking p2p

---
+++ MPI并行编程/examples/08-isend-recv.c
@@ -0,0 +1,66 @@
+
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    MPI_Comm comm = MPI_COMM_WORLD;
+    int rank, nprocs;
+    int *sb = NULL;
+    int *rb = NULL;
+    MPI_Status status;
+    MPI_Request req;
+
+    /* 100 M */
+    int size = 1024 * 1024 * 100;
+    int i;
+
+    /* initialize */
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_size(comm, &nprocs);
+    MPI_Comm_rank(comm, &rank);
+
+    if (nprocs < 2) {
+        printf("At least 2 MPIs are required for this example\n");
+        goto end;
+    }
+
+    rb = malloc(size * sizeof(*rb));
+
+    /* process 0 sends info to 1 */
+    if (rank == 0) {
+        int flag = 0;
+
+        sb = malloc(size * sizeof(*sb));
+
+        for (i = 0; i < size; i++) sb[i] = size - 1 - i;
+
+        /* send */
+        MPI_Isend(sb, size, MPI_INT, 1, 999, comm, &req);
+
+        /* test if Isend completes */
+        MPI_Test(&req, &flag, &status);
+
+        if (flag) {
+            printf("send completed\n");
+        }
+        else {
+            printf("send not completed yet. now force send to complete\n");
+
+            /* force to complete */
+            MPI_Wait(&req, &status);
+        }
+    }
+    else if (rank == 1) {
+        MPI_Recv(rb, size, MPI_INT, 0, 999, comm, &status);
+
+        printf("received: %d %d\n", rb[0], rb[1]);
+    }
+
+end:
+    MPI_Finalize();
+
+    return 0;
+}
