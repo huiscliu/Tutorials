ccc03c0a835250b9e7b42c2e45b4f5d54a3119bf
huiscliu <username@users.noreply.github.com>
2020-11-01 14:48:53 -0700

    add barrier

---
+++ MPI并行编程/examples/09-barrier.c
@@ -0,0 +1,33 @@
+
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+    MPI_Comm comm = MPI_COMM_WORLD;
+    int rank;
+
+    /* initialize */
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_rank(comm, &rank);
+
+    if (rank == 0) {
+        printf("rank %d is going to sleep\n", rank);
+        sleep(10);
+        printf("rank %d done sleeping\n", rank);
+    }
+
+    if (rank != 0) printf("rank %d is waiting for 0 to wake up\n", rank);
+
+    /* all wait here */
+    MPI_Barrier(comm);
+
+    if (rank != 0) printf("rank %d done waiting\n", rank);
+
+    MPI_Finalize();
+    return 0;
+}
+
