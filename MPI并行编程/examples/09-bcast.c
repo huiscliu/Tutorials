18b25a28cad259a63f98a1a6d62279f5b76224cb
huiscliu <username@users.noreply.github.com>
2020-11-01 20:32:02 -0700

    add bcast

---
+++ MPI并行编程/examples/09-bcast.c
@@ -0,0 +1,32 @@
+
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+    MPI_Comm comm = MPI_COMM_WORLD;
+    int rank;
+    int v = 3;
+
+    /* initialize */
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_rank(comm, &rank);
+
+    printf("rank %d, initial v is %d\n", rank, v);
+
+    if (rank == 0) {
+        v = 0;
+        printf("rank %d changed v to %d\n", rank, v);
+    }
+
+    MPI_Bcast(&v, 1, MPI_INT, 0, comm);
+
+    printf("rank %d, v is %d\n", rank, v);
+
+    MPI_Finalize();
+    return 0;
+}
+
