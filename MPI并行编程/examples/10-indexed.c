5fd4c3db62e4b4670143ab772554f0055f123bab
huiscliu <username@users.noreply.github.com>
2020-11-07 14:51:00 -0700

    add indexed

---
+++ MPI并行编程/examples/10-indexed.c
@@ -0,0 +1,47 @@
+
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    MPI_Comm comm = MPI_COMM_WORLD;
+    int rank, nprocs;
+    int sb[6] = {1, 1, 1, 1, 1, 1};
+    int rb[6] = {0, 0, 0, 0, 0, 0};
+    int blk[2] = {2, 1};
+    int std[2] = {0, 4};
+    MPI_Status status;
+    MPI_Datatype newtype;
+
+    /* initialize */
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_size(comm, &nprocs);
+    MPI_Comm_rank(comm, &rank);
+
+    if (nprocs < 2) {
+        printf("At least 2 MPIs are required for this example\n");
+        goto end;
+    }
+
+    /* new type, send 0-th, 1-th, 4-th elements */
+    MPI_Type_indexed(2, blk, std, MPI_INT, &newtype);
+    MPI_Type_commit(&newtype);
+
+    /* process 0 sends info to 1 */
+    if (rank == 0) {
+        MPI_Send(sb, 1, newtype, 1, 999, comm);
+    }
+    else if (rank == 1) {
+        MPI_Recv(rb, 1, newtype, 0, 999, comm, &status);
+
+        printf("received: %d %d %d %d %d %d\n", rb[0], rb[1], rb[2], rb[3], rb[4], rb[5]);
+    }
+
+    MPI_Type_free(&newtype);
+
+end:
+    MPI_Finalize();
+
+    return 0;
+}
