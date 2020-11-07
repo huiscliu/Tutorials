2ea4dd40c1e479fdffc0ff965ee041e15f624c6d
huiscliu <username@users.noreply.github.com>
2020-11-07 11:28:51 -0700

    contiguous

---
+++ MPI并行编程/examples/10-contiguous.c
@@ -0,0 +1,44 @@
+
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    MPI_Comm comm = MPI_COMM_WORLD;
+    int rank, nprocs;
+    int sb[4] = {1, 1, 1, 1};
+    int rb[4] = {0, 0, 0, 0};
+    MPI_Status status;
+    MPI_Datatype newtype;
+
+    /* initialize */
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_size(comm, &nprocs);
+    MPI_Comm_rank(comm, &rank);
+
+    if (nprocs < 2) {
+        printf("At least 2 MPIs are required for this example\n");
+        goto end;
+    }
+
+    MPI_Type_contiguous(4, MPI_INT, &newtype);
+    MPI_Type_commit(&newtype);
+
+    /* process 0 sends info to 1 */
+    if (rank == 0) {
+        MPI_Send(sb, 1, newtype, 1, 999, comm);
+    }
+    else if (rank == 1) {
+        MPI_Recv(rb, 1, newtype, 0, 999, comm, &status);
+
+        printf("received: %d %d %d %d\n", rb[0], rb[1], rb[2], rb[3]);
+    }
+
+    MPI_Type_free(&newtype);
+
+end:
+    MPI_Finalize();
+
+    return 0;
+}
