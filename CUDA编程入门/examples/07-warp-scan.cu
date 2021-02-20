b2a83708e558ea0d64571ca6d868d4512c4b3fae
huiscliu <username@users.noreply.github.com>
2021-02-20 15:02:31 -0700

    add warp

---
+++ CUDA编程入门/examples/07-warp-scan.cu
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+__global__ void scan4()
+{
+    int laneId = threadIdx.x & 0x1f;
+    // Seed sample starting value (inverse of lane ID)
+    int value = 31 - laneId;
+
+    // Loop to accumulate scan within my partition.
+    // Scan requires log2(n) == 3 steps for 8 threads
+    // It works by an accumulated sum up the warp
+    // by 1, 2, 4, 8 etc. steps.
+    for (int i=1; i<=4; i*=2) {
+        // We do the __shfl_sync unconditionally so that we
+        // can read even from threads which won't do a
+        // sum, and then conditionally assign the result.
+        int n = __shfl_up_sync(0xffffffff, value, i, 8);
+
+        if ((laneId & 7) >= i) value += n;
+    }
+
+    printf("Thread %d final value = %d\n", threadIdx.x, value);
+}
+
+int main()
+{
+    scan4<<< 1, 32 >>>();
+    cudaDeviceSynchronize();
+
+    return 0;
+}
