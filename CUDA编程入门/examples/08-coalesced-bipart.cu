004de5d9cde869bb5ca16adff996adce0f27a4c7
huiscliu <username@users.noreply.github.com>
2021-02-20 16:23:15 -0700

    cg

---
+++ CUDA编程入门/examples/08-coalesced-bipart.cu
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <cuda.h>
+
+/* cg */
+#include <cooperative_groups.h>
+using namespace cooperative_groups;
+
+__global__ void kernel()
+{
+    int oe = threadIdx.x & 0x1;  /* odd or even */
+
+    /* include all threads */
+    coalesced_group g1 = coalesced_threads();
+    unsigned int mask = __activemask();
+
+    /* g2, g3 and g4 are equivalent */
+    coalesced_group g3 = binary_partition(g1, oe);
+    coalesced_group g4 = labeled_partition(g1, oe);
+
+    if (oe) {
+        coalesced_group g2 = coalesced_threads();
+        mask = __activemask();
+    }
+    else {
+        coalesced_group g2 = coalesced_threads();
+        mask = __activemask();
+    }
+
+    printf("Thread %d final mask = %ud\n", threadIdx.x, mask);
+}
+
+int main()
+{
+    kernel<<< 1, 32 >>>();
+    cudaDeviceSynchronize();
+
+    return 0;
+}
---
+++ CUDA编程入门/examples/08-coalesced-label-part.cu
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <cuda.h>
+
+/* cg */
+#include <cooperative_groups.h>
+using namespace cooperative_groups;
+
+__global__ void kernel()
+{
+    int oe = threadIdx.x % 5;
+
+    /* include all threads */
+    coalesced_group g1 = coalesced_threads();
+    unsigned int mask = __activemask();
+
+    /* g3 and g2 are equivalent */
+    coalesced_group g3 = labeled_partition(g1, oe);
+
+    if (oe == 0) {
+        coalesced_group g2 = coalesced_threads();
+        mask = __activemask();
+    }
+    else if (oe == 1) {
+        coalesced_group g2 = coalesced_threads();
+        mask = __activemask();
+    }
+    else if (oe == 2) {
+        coalesced_group g2 = coalesced_threads();
+        mask = __activemask();
+    }
+    else if (oe == 3) {
+        coalesced_group g2 = coalesced_threads();
+        mask = __activemask();
+    }
+    else {
+        coalesced_group g2 = coalesced_threads();
+        mask = __activemask();
+    }
+
+    printf("Thread %d final mask = %ud\n", threadIdx.x, mask);
+}
+
+int main()
+{
+    kernel<<< 1, 32 >>>();
+    cudaDeviceSynchronize();
+
+    return 0;
+}
