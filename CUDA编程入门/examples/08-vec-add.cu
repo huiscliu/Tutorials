62a69eb3b58d5d831ed7c3e0eba3e072d3a77ae5
huiscliu <username@users.noreply.github.com>
2021-02-20 15:59:33 -0700

    cg

---
+++ CUDA编程入门/examples/08-vec-add.cu
@@ -0,0 +1,157 @@
+
+#include <stdio.h>
+#include <cuda.h>
+
+/* cg */
+#include <cooperative_groups.h>
+using namespace cooperative_groups;
+
+typedef float FLOAT;
+
+/* get time stamp */
+double get_time(void);
+
+/* host, add */
+void vec_add_host(FLOAT *x, FLOAT *y, FLOAT *z, int N);
+
+/* device function */
+__global__ void vec_add(FLOAT *x, FLOAT *y, FLOAT *z, int N)
+{
+    grid_group g = this_grid();
+    int idx = g.thread_rank();
+
+    if (idx < N) z[idx] = z[idx] + y[idx] + x[idx];
+}
+
+void vec_add_host(FLOAT *x, FLOAT *y, FLOAT *z, int N)
+{
+    int i;
+
+    for (i = 0; i < N; i++) z[i] = z[i] + y[i] + x[i];
+}
+
+/* a little system programming */
+#if defined(__linux__) || defined(__APPLE__)
+
+#include <sys/time.h>
+#include <time.h>
+
+double get_time(void)
+{
+    struct timeval tv;
+    double t;
+
+    gettimeofday(&tv, (struct timezone *)0);
+    t = tv.tv_sec + (double)tv.tv_usec * 1e-6;
+
+    return t;
+}
+
+#elif defined(_WIN32)
+
+#include <windows.h>
+
+double get_time(void)
+{
+    LARGE_INTEGER timer;
+    static LARGE_INTEGER fre;
+    static int init = 0;
+    double t;
+
+    if (init != 1) {
+        QueryPerformanceFrequency(&fre);
+        init = 1;
+    }
+
+    QueryPerformanceCounter(&timer);
+
+    t = timer.QuadPart * 1. / fre.QuadPart;
+
+    return t;
+}
+
+#else
+#error "unknown OS"
+#endif
+
+int main()
+{
+    int N = 20000000;
+    int nbytes = N * sizeof(FLOAT);
+
+    /* 1D block */
+    int bs = 256;
+
+    /* 2D grid */
+    int s = ceil(sqrt((N + bs - 1.) / bs));
+    dim3 grid = dim3(s, s);
+
+    FLOAT *dx = NULL, *hx = NULL;
+    FLOAT *dy = NULL, *hy = NULL;
+    FLOAT *dz = NULL, *hz = NULL;
+
+    int itr = 30;
+    int i;
+    double th, td;
+
+    /* allocate GPU mem */
+    cudaMalloc((void **)&dx, nbytes);
+    cudaMalloc((void **)&dy, nbytes);
+    cudaMalloc((void **)&dz, nbytes);
+
+    if (dx == NULL || dy == NULL || dz == NULL) {
+        printf("couldn't allocate GPU memory\n");
+        return -1;
+    }
+
+    printf("allocated %.2f MB on GPU\n", nbytes / (1024.f * 1024.f));
+
+    /* alllocate CPU mem */
+    hx = (FLOAT *) malloc(nbytes);
+    hy = (FLOAT *) malloc(nbytes);
+    hz = (FLOAT *) malloc(nbytes);
+
+    if (hx == NULL || hy == NULL || hz == NULL) {
+        printf("couldn't allocate CPU memory\n");
+        return -2;
+    }
+    printf("allocated %.2f MB on CPU\n", nbytes / (1024.f * 1024.f));
+
+    /* init */
+    for (i = 0; i < N; i++) {
+        hx[i] = 1;
+        hy[i] = 1;
+        hz[i] = 1;
+    }
+
+    /* copy data to GPU */
+    cudaMemcpy(dx, hx, nbytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(dy, hy, nbytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(dz, hz, nbytes, cudaMemcpyHostToDevice);
+
+    /* call GPU */
+    cudaDeviceSynchronize();
+    td = get_time();
+    
+    for (i = 0; i < itr; i++) vec_add<<<grid, bs>>>(dx, dy, dz, N);
+
+    cudaDeviceSynchronize();
+    td = get_time() - td;
+
+    /* CPU */
+    th = get_time();
+    for (i = 0; i < itr; i++) vec_add_host(hx, hy, hz, N);
+    th = get_time() - th;
+
+    printf("GPU time: %e, CPU time: %e, speedup: %g\n", td, th, th / td);
+
+    cudaFree(dx);
+    cudaFree(dy);
+    cudaFree(dz);
+
+    free(hx);
+    free(hy);
+    free(hz);
+
+    return 0;
+}
